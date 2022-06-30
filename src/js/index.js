6c14a90a6c69d780e20d0b7e3fc1c3e35cd66a75
mikhail <VO3@Pgasn>
2022-07-01 00:56:02 +0300

    fetch: add search

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,35 @@
+import {render} from "./ui.js"
+
 async function getResponse() {
   let response = await fetch(
     "https://62b4de4cda3017eabb120a85.mockapi.io/Post"
   );
-  let content = await response.json();
-  content = content.splice(0, 12);
+  let products = await response.json();
+  products = products.splice(0, 12);
+  
+  render(products);
+ 
+  let search = document.querySelector(".header__search");
+  
+  let searchProducts =  [];
+  console.log(search);
+  
+  search.addEventListener("keydown", (event) => {    
+    if (search.value === "") return;
+    if (event.code == "Enter"){
+      searchProducts = products.filter(product => (product.name.includes(search.value))); 
+      console.log(searchProducts);
+      render(searchProducts);
+      console.log(render(searchProducts));
+    }
+  }); 
 
-  let card = document.querySelector(".products");
+  let showAll = document.querySelector(".bestseller");
 
-  let key;
-
-  for (key in content) {
-    card.innerHTML += `
-        <div class="products__cart">
-          <div class="products__photo">
-            <img
-                 src="${content[key].image}"
-                 alt="clothes"
-                 class="photo"
-            />
-            <div class="overlay">
-              <button class="overlay__viewing">Быстрый просмотр</button>
-              <span class="overlay__discount">-10%</span>
-              <button class="overlay__basket">+</button>
-            </div>
-            </div>
-            <div class="products__info">
-            <div class="products__price">
-            <span class="products__price-new">${content[key].price}</span>
-            <span class="products__price-old"><s>${content[key].prePrice}</s></span>
-            </div>
-            <p class="products__name">${content[key].name}</p>
-          </div>
-        </div>
-    `;
+  showAll.addEventListener("click", () => {    
+    render(products);
   }
+  );
 }
+
 getResponse();
