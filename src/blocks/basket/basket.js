5226c75495937120126a798ad474b37c091d46d4
mikhail <VO3@Pgasn>
2022-07-03 18:19:35 +0300

    fetch: add products in the  basket

--- src/blocks/basket/basket.js
+++ src/blocks/basket/basket.js
@@ -1,95 +1,64 @@
+const json = localStorage.getItem("products") || "[]";
+console.log(json);
+const products = JSON.parse(json);
+
 const btn = document.getElementById("myBtn");
 const modal = document.getElementById("myModal");
-modal.innerHTML = `
-<div class="modal__content">
-<h2>Корзина товаров</h2>
-<div id="cart_content"></div>
-<p class="summa">Итого:</p>
-<button id="clear_cart">Очистить корзину</button>
-<div class="modal__basket"></div>
-</div>`;
-let d = document,
-  itemBox = d.querySelectorAll(".products__cart"),
-  cartCont = d.getElementById("cart_content");
 
-btn.onclick = function () {
-  modal.style.display = "block";
-};
 
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-};
-
-function addEvent(elem, type, handler) {
-  if (elem.addEventListener) {
-    elem.addEventListener(type, handler, false);
-  } else {
-    elem.attachEvent("on" + type, function () {
-      handler.call(elem);
+const ProductManager = {
+  products: products,
+  generateModalContent() {
+    modal.innerHTML = `
+    <div class="modal__content">
+      <h2>Корзина товаров</h2>
+      <div id="cart_content"></div>
+      <p class="summa">Итого:</p>
+      <button id="clear_cart">Очистить корзину</button>
+      <div class="modal__basket"></div>
+    </div>`;    
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
     });
-  }
-  return false;
-}
-
-function getCartData() {
-  return JSON.parse(localStorage.getItem("cart"));
-}
-
-function setCartData(o) {
-  localStorage.setItem("cart", JSON.stringify(o));
-  return false;
-}
-
-function addToCart(e) {
-  this.disabled = true;
-  let cartData = getCartData() || {},
-    parentBox = this.parentNode,
-    itemId = this.getAttribute("data-id"),
-    itemTitle = parentBox.querySelector(".products__name").innerHTML,
-    itemPrice = parentBox.querySelector(".products__price-new").innerHTML;
-  if (cartData.hasOwnProperty(itemId)) {
-    cartData[itemId][2] += 1;
-  } else {
-    cartData[itemId] = [itemTitle, itemPrice, 1];
-  }
-  if (!setCartData(cartData)) {
-    this.disabled = false;
-  }
-  return false;
-}
-for (let i = 0; i < itemBox.length; i++) {
-  addEvent(itemBox[i].querySelector(".overlay__basket"), "click", addToCart);
-}
-
-function openCart(e) {
-  let cartData = getCartData(),
-    totalItems = "";
-  if (cartData !== null) {
-    totalItems =
-      '<table class="shopping_list"><tr><th>Наименование</th><th>Цена</th><th>Кол-во</th></tr>';
-    for (let items in cartData) {
-      totalItems += "<tr>";
-      for (let i = 0; i < cartData[items].length; i++) {
-        totalItems += "<td>" + cartData[items][i] + "</td>";
+    
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
       }
-      totalItems += "</tr>";
-    }
-    totalItems += "</table>";
-    cartCont.innerHTML = totalItems;
-  } else {
-    cartCont.innerHTML = "В корзине пусто!";
-  }
-  return false;
-}
+    });
+    
+    const clear = document.getElementById("clear_cart");
 
-addEvent(d.getElementById("myBtn"), "click", openCart);
-addEvent(d.getElementById("clear_cart"), "click", function (e) {
-  localStorage.removeItem("cart");
-  let basket = document.querySelector(".modal__basket");
-  console.log(basket);
-  console.log('ура');
-  basket.innerHTML ="";
-  cartCont.innerHTML = "Корзина очищена.";
-});
+    clear.addEventListener("click", () => {
+      let cartCont = document.getElementById("cart_content");
+      let basket = document.querySelector(".modal__basket");
+      let result = document.querySelector(".summa");
+      localStorage.clear();
+      ProductManager.products = [];
+      basket.innerHTML ="";
+      result.innerHTML = "";
+      cartCont.innerHTML = "Корзина очищена.";      
+    }) 
+  },
+   
+  add(id, link, name, price, prePrice){
+    let product ={
+      id: id,
+      image: link,
+      name: name,
+      price: price,
+      prePrice: prePrice,
+    };
+
+    this.products.push(product);
+    this.saveToLocalStorage();
+  },
+
+  saveToLocalStorage(){
+    const json = JSON.stringify(this.products);
+    localStorage.setItem("products", json);
+  }  
+};
+
+export default ProductManager;
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,71 @@
-import {render} from "./ui.js"
+import ProductManager from "../blocks/basket/basket.js";
+import {render} from "./ui.js";
+
 
 async function getResponse() {
+  let container = document.querySelector(".products");
+  console.log(container);
   let response = await fetch(
     "https://62b4de4cda3017eabb120a85.mockapi.io/Post"
   );
   let products = await response.json();
-  products = products.splice(0, 12);
+
+  let basket = document.querySelector(".modal__basket");
+  products = products.splice(0, 12);  
   
-  render(products);
- 
+  
+  function summa(products) {
+    let sum = 0;
+    for(let product of products){
+      sum+= +product.price;
+    }
+
+    let result = document.querySelector(".summa");
+    result.innerHTML = `<h2>Общая сумма покупок:${sum}</h2>`;    
+  }
+
+  summa(ProductManager.products);
+
+  render(container, products);  
+  render(basket, ProductManager.products, "false");
+
   let search = document.querySelector(".header__search");
-  
-  let searchProducts =  [];  
-  
+  let searchProducts =  []; 
+
   search.addEventListener("keydown", (event) => {    
     if (search.value === "") return;
     if (event.code == "Enter"){
       searchProducts = products.filter(product => (product.name.includes(search.value))); 
-      console.log(searchProducts);
-      render(searchProducts);
-      console.log(render(searchProducts));
+      render(container, searchProducts);
     }
   }); 
-
-  let container = document.querySelector(".products");
-  
-  let basket = document.querySelector(".modal__basket");
-  
+    
   container.addEventListener("click", (event) => {   
     let target = event.target;
-    let cart = target.closest('.products__cart');
+    let cart = target.closest(".products__cart");
+    let id = cart.getAttribute("data-id")
+    let img = cart.querySelector(".photo");
+    let link = img.getAttribute("src");
+    let name = cart.getAttribute("data-name");
+    let price = cart.getAttribute("data-price");
+    let prePrice = cart.getAttribute("data-prePrice");
+    
     if (target.className != "overlay__basket") return;    
-    let overlay = target.closest(".overlay");
-    overlay.style.display = "none";
-    console.log(cart);
     basket.append(cart);
-    }); 
-
+    
+    ProductManager.add(id, link, name, price, prePrice);
+    ProductManager.saveToLocalStorage();
+    summa(ProductManager.products);
+  }); 
   
   let showAll = document.querySelector(".bestseller");
-  console.log(showAll);
-
+  
   showAll.addEventListener("click", () => {    
-    render(products);
+    render(container, products);
   }
   );
 }
 
 getResponse();
+ProductManager.generateModalContent();
+
